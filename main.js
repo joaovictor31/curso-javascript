ccc7749271fdcb39a04f4b334c424c9a17a99f36
João Victor <101882992+joaovictor31@users.noreply.github.com>
2023-09-08 19:31:12 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//01 - Retorne todas as imagens do site.
+const imagens = document.querySelectorAll('img');
+console.log(imagens);
+
+//02- Retorne no console as imagens que começarem com a palavra imagem.
+const soAnimais = document.querySelectorAll('img[src^="img/slide"]')
+console.log(soAnimais);
+
+//03 Selecionar todos os links internos onde o href começa com # parei em11:14
