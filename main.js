e78be41e30d69f2564d2d28039316420de825fe9
João Victor <101882992+joaovictor31@users.noreply.github.com>
2023-09-08 21:06:36 -0300

    Novas
    Atualizações

--- main.js
+++ main.js
@@ -6,4 +6,10 @@ console.log(imagens);
 const soAnimais = document.querySelectorAll('img[src^="img/slide"]')
 console.log(soAnimais);
 
-//03 Selecionar todos os links internos onde o href começa com # parei em11:14
+//03 Selecionar todos os links internos onde o href começa com # 
+const linksInternos = document.querySelectorAll('[href^="#"]');
+console.log(linksInternos);
+
+//04 Selecionar o primeiro h2 dentro de .animais-descricao
+const h2Animais = document.querySelector('.animais-descricao h2');
+console.log(h2Animais);
